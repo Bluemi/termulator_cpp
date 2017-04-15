c14cd15e19e31b722d799d32dc8613e96e5b3b07
Bluemi <bruno.schilling@protonmail.ch>
2017-04-15 12:19:36 +0200

    -+Value

--- src/core/values/Value.cpp
+++ src/core/values/Value.cpp
@@ -1,24 +1,43 @@
 #include "Value.hpp"
 
 #include <iostream>
+#include <tgmath.h>
 
 Value::Value(const Value::Behavior behavior)
-	: behavior_(behavior)
+	: behavior_(behavior), coefficient_(0), exponent_(0)
 {}
 
 Value::~Value()
 {}
 
-
 // Approx
 double Value::getApprox() const
 {
-	return (double)numerator_/(double)denominator_;
+	return coefficient_ * pow(10.0, exponent_);
 }
 
 void Value::setApprox(const double value)
 {
 	std::cout << "Value::setApprox(): TODO" << std::endl;
+}
+
+std::string Value::getString() const
+{
+	std::cout << "Value::getString(): TODO" << std::endl;
+	return "";
+}
+
+// parse
+bool parseInt(const std::string& s)
+{
+	std::cout << "Value::parseInt(): TODO" << std::endl;
+	return false;
+}
+
+bool Value::parseFloat(const std::string& s)
+{
+	std::cout << "Value::parseInt(): TODO" << std::endl;
+	return false;
 }
 
 // Behavior
@@ -33,88 +52,32 @@ void Value::setBehavior(const Value::Behavior behavior)
 }
 
 // operations
-
-void Value::optimize()
-{
-	std::cout << "Value::optimize(): TODO" << std::endl;
-}
-
-Value::Behavior mergeBehaviors(const Value::Behavior& a, const Value::Behavior& b)
-{
-	if ((a == Value::Behavior::FLOAT_BEHAVIOR) || (b == Value::Behavior::FLOAT_BEHAVIOR))
-	{
-		return Value::Behavior::FLOAT_BEHAVIOR;
-	}
-	return Value::Behavior::INT_BEHAVIOR;
-}
-
 Value Value::operator+(const Value& value) const
 {
-	Value::Behavior newBehavior = mergeBehaviors(this->behavior_, value.behavior_);
-	Value v(newBehavior);
-	if (newBehavior == Value::Behavior::INT_BEHAVIOR)
-	{
-		v.numerator_ = this->numerator_ + value.numerator_;
-		v.denominator_ = 1;
-		return v;
-	}
-	
-	v.denominator_ = this->denominator_ * value.denominator_;
-	v.numerator_ = this->denominator_ * value.numerator_ + this->numerator_ * value.denominator_;
-	return v;
+	std::cout << "Value::operator+(): TODO" << std::endl;
+	return Value(INT_BEHAVIOR);
 }
 
 Value Value::operator-(const Value& value) const
 {
-	Value::Behavior newBehavior = mergeBehaviors(this->behavior_, value.behavior_);
-	Value v(newBehavior);
-	if (newBehavior == Value::Behavior::INT_BEHAVIOR)
-	{
-		v.numerator_ = this->numerator_ - value.numerator_;
-		v.denominator_ = 1;
-		return v;
-	}
-	
-	v.denominator_ = this->denominator_ * value.denominator_;
-	v.numerator_ =  this->numerator_ * value.denominator_ - this->denominator_ * value.numerator_;
-	return v;
+	std::cout << "Value::operator+(): TODO" << std::endl;
+	return Value(INT_BEHAVIOR);
 }
 
 Value Value::operator*(const Value& value) const
 {
-	Value::Behavior newBehavior(mergeBehaviors(this->behavior_, value.behavior_));
-	Value v(newBehavior);
-	if (newBehavior == Value::Behavior::INT_BEHAVIOR)
-	{
-		v.numerator_ = this->numerator_ * value.numerator_;
-		v.denominator_ = 1;
-		return v;
-	}
-	v.numerator_ = this->numerator_ * value.numerator_;
-	v.denominator_ = this->denominator_ * value.denominator_;
-	return v;
+	std::cout << "Value::operator+(): TODO" << std::endl;
+	return Value(INT_BEHAVIOR);
 }
 
 Value Value::operator/(const Value& value) const
 {
-	if (value.numerator_ == 0)
-	{
-		std::cout << "Value::operator/(): division by zero Error" << std::endl;
-	}
-	Behavior newBehavior(mergeBehaviors(this->behavior_, value.behavior_));
-	Value v(newBehavior);
-	if (newBehavior == Value::Behavior::INT_BEHAVIOR)
-	{
-		v.numerator_ = this->numerator_ / value.numerator_;
-		v.denominator_ = 1;
-		return v;
-	}
-	v.numerator_ = this->numerator_ * value.denominator_;
-	v.denominator_ = this->denominator_ * value.numerator_;
-	return v;
+	std::cout << "Value::operator+(): TODO" << std::endl;
+	return Value(INT_BEHAVIOR);
 }
 
 bool Value::operator==(const Value& value) const
 {
-	return (this->numerator_ == value.numerator_) && (this->denominator_ == value.denominator_);
+	std::cout << "Value::operator+(): TODO" << std::endl;
+	return false;
 }
--- src/core/values/Value.hpp
+++ src/core/values/Value.hpp
@@ -1,6 +1,13 @@
 #ifndef __VALUE_CLASS__
 #define __VALUE_CLASS__
 
+/*
+	represents a human writable number like 0.5 or 0.123, if behavior is equal to "FLOAT_BEHAVIOR" or represents ints, if behavior is set to "INT_BEHAVIOR".
+	Cannot represent rational numbers like 1/3 exactly.
+	The represented value is equal to Coefficient * 10 ^ Exponent.
+*/
+
+#include <string>
 
 class Value
 {
@@ -15,12 +22,17 @@ class Value
 		double getApprox() const;
 	 	void setApprox(const double value);
 
+		std::string getString() const;
+
+		// parse
+		bool parseInt(const std::string& s);
+		bool parseFloat(const std::string& s);
+
 		// Behavior
 		Behavior getBehavior() const;
 		void setBehavior(const Behavior behavior);
 
 		// operations
-		void optimize();
 		Value operator+(const Value& value) const;
 		Value operator-(const Value& value) const;
 		Value operator*(const Value& value) const;
@@ -28,8 +40,8 @@ class Value
 		bool operator==(const Value& value) const;
 	private:
 		Behavior behavior_;
-		long int numerator_;
-		long int denominator_;
+		long int coefficient_;
+		long int exponent_;
 };
 
 #endif
